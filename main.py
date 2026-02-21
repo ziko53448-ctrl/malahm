5818012c543de3f523de3467600a55ec0f4f879c
ziko53448-ctrl <ziko53448@gmail.com>
2026-02-21 01:46:02 -0500

    Create main Flask application file

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+# Define database models and routes
+
+if __name__ == '__main__':
+    app.run(debug=True)
